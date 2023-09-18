3d552c9055776c936a8927b075621e982f402565
Fady <Fady.soly55@gmail.com>
2023-09-19 02:04:54 +0300

    updating Nigga.cpp

--- Nigga.cpp
+++ Nigga.cpp
@@ -1,60 +1,33 @@
 #include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
 #define Nigga_ ios::sync_with_stdio(false), cin.tie(0), cout.tie(0);
 #define all(x) x.begin(), x.end()
 //#define int long long
-//test
 typedef long long ll;
 const int MOD = 1e9 + 7;
 const short dx[] = {-1, 0, 0, 1, 1, -1, 1, -1};
 const short dy[] = {0, -1, 1, 0, 1, -1, -1, 1};
 const char dc[] = {'U', 'L', 'R', 'D'};
 using namespace std;
+using namespace __gnu_pbds;
+//----------------------------------------------------------------------------------
+template<typename K, typename V, typename Comp = less<K>>
+using ordered_map = tree<K, V, Comp, rb_tree_tag, tree_order_statistics_node_update>;
+template<typename K, typename Comp = less<K>>
+using ordered_set = ordered_map<K, null_type, Comp>;
+
+template<typename K, typename V, typename Comp = less_equal<K>>
+using ordered_multimap = tree<K, V, Comp, rb_tree_tag, tree_order_statistics_node_update>;
+template<typename K, typename Comp = less_equal<K>>
+using ordered_multiset = ordered_multimap<K, null_type, Comp>;
+//----------------------------------------------------------------------------------
 template<typename T> using min_queue = priority_queue<T, vector<T>, greater<>>;
-
-struct segtree {
-    int size = 1;
-    vector<ll> sums;
-    explicit segtree(vector<int> &arr) {
-        while(size < arr.size()) size <<= 1;
-        sums.resize(size << 1, 0LL);
-        build(arr, 0, 0, size);
-    }
-    void build(vector<int> &arr, int x, int lx, int rx) {
-        if(lx + 1 == rx) {
-            sums[x] = arr[lx];
-            return;
-        }
-        int m = (lx + rx) >> 1;
-        build(arr, 2 * x + 1, lx, m);
-        build(arr, 2 * x + 2, m, rx);
-        sums[x] = sums[2 * x + 1] + sums[2 * x + 2];
-    }
-    void set(int i, int v, int x, int lx, int rx) {
-        if(lx + 1 == rx) {
-            sums[x] = v;
-            return;
-        }
-        int m = (lx + rx) >> 1;
-        if(i < m)
-            set(i, v, 2 * x + 1, lx, m);
-        else
-            set(i, v, 2 * x + 2, m, rx);
-        sums[x] = sums[2 * x + 1] + sums[2 * x + 2];
-    }
-    ll sum(int l, int r, int x, int lx, int rx) {
-        if(r <= lx || rx <= l) return 0;
-        if(lx >= l && rx <= r) return sums[x];
-
-        int m = (lx + rx) >> 1;
-        return sum(l, r, 2 * x + 1, lx, m) + sum(l, r, 2 * x + 2, m, rx);
-    }
-};
 
 template<typename T> struct nT {
     T mod;
     vector <T> fac;
-    vector<bool> Sieve;
-    vector<int> primes;
+    vector<int> Sieve;
 
     explicit nT(T m = 1e9 + 7) : mod(m) {}
     void buildFac(int numberOfFactorial){
@@ -64,16 +37,12 @@ template<typename T> struct nT {
             fac[i] = ((__int128) fac[i - 1] * i) % mod;
     }
     void buildSieve(int sieve){
-        Sieve.resize(sieve + 1, true);
-        for (int i = 2; i * i <= sieve; i++)
-            if (Sieve[i]) {
-                primes.push_back(i);
-                for(int j = i + i; j <= sieve; j += i)
-                    Sieve[j] = false;
-            }
-        int m = (int)sqrt(sieve);
-        for(int i = m % 2 == 1?m:m + 1; i <= sieve; i += 2)
-            if(Sieve[i]) primes.push_back(i);
+        Sieve.resize(sieve + 1, 0);
+        for (int i = 1; i <= sieve; i++){
+            for(int j = i; j <= sieve; j += i)
+                Sieve[j]++;
+        }
+
     }
     bool isPrime(ll num) {
         if (num == 2) return true;
@@ -127,14 +96,100 @@ template<typename T> struct nT {
     }
 };
 
+
+class segtree {
+    struct node{
+        int v;
+        bool f = false;
+        int e = 0;
+        node *l, *r;
+        explicit node(int v = 1e7, node *l = nullptr, node *r = nullptr) : v(v), l(l), r(r) { }
+    };
+    void update(node *x) {
+        if(x && x->f) {
+            x->f = false;
+            x->v += x->e;
+            if(x->l)
+                x->l->f = true, x->l->e += x->e;
+            if(x->r)
+                x->r->f = true, x->r->e += x->e;
+            x->e = 0;
+        }
+    }
+    void merge(node *x) {
+        update(x), update(x->l), update(x->r);
+        x->v = min(x->l->v, x->r->v);
+    }
+    node *build(int lx, int rx) {
+        if(lx == rx)
+            return new node(arr[lx]);
+
+        int m = (lx + rx) >> 1;
+        node *l = build(lx, m), *r = build(m + 1, rx), *x = new node(1e9, l, r);
+        merge(x);
+        return x;
+    }
+    int q(node *x, int lx, int rx, int l, int r) {
+        update(x);
+        if(lx > r || l > rx)
+            return 1e9;
+        if(lx >= l && rx <= r)
+            return x->v;
+        int m = (lx + rx) >> 1;
+        return min(q(x->l, lx, m, l, r), q(x->r, m + 1, rx, l, r));
+    }
+    void setRange(node *x, int lx, int rx, int l, int r, int v) {
+        update(x);
+        if(lx > r || l > rx)
+            return;
+        if(lx >= l && rx <= r) {
+            x->f = true;
+            x->e += v;
+            update(x);
+            return;
+        }
+        int m = (lx + rx) >> 1;
+        setRange(x->l, lx, m, l, r, v);
+        setRange(x->r, m + 1, rx, l, r, v);
+        merge(x);
+    }
+    int size;
+    node *root = nullptr;
+    vector<int> &arr;
+    void del(node *x) {
+        if(x) {
+            del(x->l);
+            del(x->r);
+            delete x;
+        }
+    }
+public:
+    explicit segtree(vector<int> &arr) : arr(arr), size((int)arr.size() - 1) {
+        root = build(0, size);
+    }
+    ~segtree() {
+        del(root);
+    }
+    int Q(int l, int r) {
+        if(l <= r)
+            return q(root, 0, size, l, r);
+        return min(q(root, 0, size, 0, r), q(root, 0, size, l, size));
+    }
+    void setRange(int l, int r, int v) {
+        if(l <= r)
+            setRange(root, 0, size, l, r, v);
+        else
+            setRange(root, 0, size, 0, r, v), setRange(root, 0, size, l, size, v);
+    }
+};
+
 void go() {
-    
+
 }
 
-int32_t main()
-{
+int32_t main() {
     Nigga_
-    cout << fixed << setprecision(6);
+    cout << fixed << setprecision(10);
     //freopen("output.txt", "w", stdout); freopen("input.txt", "r", stdin);
     int test = 1; //cin >> test;
     while (test-- > 0){
