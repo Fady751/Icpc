e04502498a93699cc240e22df6b4a583cd349b0b
Fady <Fady.soly55@gmail.com>
2023-09-19 07:33:32 +0300

    .

--- Packet.cpp
+++ Packet.cpp
@@ -1,16 +1,17 @@
 #include <bits/stdc++.h>
 #define Packet ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
 //#define all(x) x.begin(), x.end()
+using ll = long long;
 using namespace std;
 
 void packet() {
-    
+
 }
 
 int32_t main() {
     Packet
     int tt = 1;
-    //cin >> tt;
+    cin >> tt;
     for (int test = 1; test <= tt; ++test) {
         packet();
     }
