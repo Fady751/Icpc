3b2115c500a557eb67fbe6e4615586188a0547d2
Fady <Fady.soly55@gmail.com>
2023-10-02 00:52:03 +0300

    .

--- test.cpp
+++ test.cpp
@@ -1,8 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class Solution {
+public:
+
+};
+
 int main() {
     ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+
+    Solution s;
 
     return 0;
 }
