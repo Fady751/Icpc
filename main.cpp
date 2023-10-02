c6eddd5ab95e491cd96c62bbfa976ece60ea2799
Fady <Fady.soly55@gmail.com>
2023-10-03 01:37:22 +0300

    .

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#define notToday ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+//#define all(x) x.begin(), x.end()
+using ll = long long;
+using namespace std;
+
+void _T_() {
+    
+}
+
+int32_t main() {
+    notToday
+    int tt = 1;
+    //cin >> tt;
+    for (int test = 1; test <= tt; ++test) {
+        _T_();
+    }
+    return 0;
+}
