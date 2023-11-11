5a00868a0a4e5c214cfb1ebd8b3dc9f32fddf93b
Fady <Fady.soly55@gmail.com>
2023-11-11 21:10:07 +0200

    .

--- main.cpp
+++ main.cpp
@@ -5,7 +5,22 @@ using ll = long long;
 using namespace std;
 
 void T() {
+    int n, m;
+    scanf("%d %d", &n, &m);
+    int arr[n];
+    for(int i = 0; i < n; i++)
+        scanf("%d", arr + i);
 
+    int l = 0, r = 0;
+    ll sum = 0;
+    while(m--) {
+        int x;
+        scanf("%d", &x);
+        while(l < n && arr[l] < x) l++;
+        while(r < n && arr[r] <= x) r++;
+        sum += r - l;
+    }
+    cout << sum << '\n';
 }
 
 int32_t main() {
