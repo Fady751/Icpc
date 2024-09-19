87e6be3cedcd344af2f0fa792de64f5d167d8d72
Fady751 <Fady.soly55@gmail.com>
2024-09-20 00:02:19 +0300

    .

--- template.cpp
+++ template.cpp
@@ -1656,6 +1656,30 @@ void sos(vector<int> &dp, bool Do = true) {
     }
 }
 
+using ld = long double;
+struct point : public array<ld, 2> {
+    ld dis(point x) {
+        return sqrt((x[0] - (*this)[0]) * (x[0] - (*this)[0])
+                    + (x[1] - (*this)[1]) * (x[1] - (*this)[1]));
+    }
+    void move(ld x, point v) {
+        v[0] *= x;
+        v[0] += (*this)[0];
+        v[1] *= x;
+        v[1] += (*this)[1];
+        (*this) = v;
+    }
+    point V(point o) {
+        if(o == *this)
+            return point{0, 0};
+        ld ds = this->dis(o);
+        return {(o[0] - (*this)[0]) / ds, (o[1] - (*this)[1]) / ds};
+    }
+    friend istream &operator>>(istream &is, point &o) {
+        return is >> o[0] >> o[1];
+    }
+};
+
 void solve() {
 
 }
