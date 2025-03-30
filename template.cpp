13e2384d2f55bfd34fca3fe893d12bd9037439eb
Fady751 <Fady.soly55@gmail.com>
2025-03-30 18:16:45 +0200

    update FFT

--- template.cpp
+++ template.cpp
@@ -430,60 +430,49 @@ struct sparse{
 };
 
 namespace RollingHash {
-    int b1 = 31, b2 = 69, mod = 1e9 + 7, b1I = 129032259, b2I = 579710149;
-    vector<int> Pb1, Pb2;
-    using pi = pair<int, int>;
+    const int N = 200'000, b1 = 31, b2 = 69, mod = 1e9 + 7, b1I = 129032259, b2I = 579710149;
 
-    void pre(unsigned maxSize) {
-        Pb1 = Pb2 = vector<int>(maxSize + 1, 1);
+    vector<int> Pb1(N + 1), Pb2(N + 1);
+    char init = []() {
         Pb1[1] = b1, Pb2[1] = b2;
-        for (int i = 2; i <= maxSize; i++) {
+        for (int i = 2; i <= N; i++) {
             Pb1[i] = int(1LL * Pb1[i - 1] * b1 % mod);
             Pb2[i] = int(1LL * Pb2[i - 1] * b2 % mod);
         }
-    }
-    struct Hash {
-        pair<int, int> code;
-        int size;
-        explicit Hash(pair<int, int> x = {}, int sz = {}) : code(std::move(x)), size(sz) { }
+        return char();
+    }();
 
-        Hash(int x) : code({x % mod, x % mod}), size(1) { }
+    struct Hash : array<int, 3> {
+#define me (*this)
+        Hash() : array() { }
+        Hash(const array<int, 3> &x) : array(x) { }
+        Hash(int x) : array({x % mod, x % mod, 1}) { }
 
-        Hash(const string &x) : code(), size(0) {
+        Hash(const string &x) : array() {
             for(char c : x) *this = *(this) + c;
         }
 
         void pop_front(int x) {
-            code.first = int((code.first - 1LL * Pb1[--size] * x % mod + mod) % mod);
-            code.second = int((code.second - 1LL * Pb2[size] * x % mod + mod) % mod);
+            me[0] = int((me[0] - 1LL * Pb1[--me[2]] * x % mod + mod) % mod);
+            me[1] = int((me[1] - 1LL * Pb2[me[2]] * x % mod + mod) % mod);
         }
 
         void pop_back(int x) {
-            code.first = int((1LL * (code.first - x + mod) * b1I) % mod);
-            code.second = int((1LL * (code.second - x + mod) * b2I) % mod);
-            size--;
+            me[0] = int((1LL * (me[0] - x + mod) * b1I) % mod);
+            me[1] = int((1LL * (me[1] - x + mod) * b2I) % mod);
+            me[2]--;
         }
 
         void clear() {
-            code = {}, size = 0;
+            me[0] = me[1] = me[2] = 0;
         }
 
         friend Hash operator+(const Hash &f, const Hash &o) {
-            return Hash({int((1LL * f.code.first * Pb1[o.size] + o.code.first) % mod),
-                         int((1LL * f.code.second * Pb2[o.size] + o.code.second) % mod)}
-                    , f.size + o.size);
+            return array{int((1LL * f[0] * Pb1[o[2]] + o[0]) % mod),
+                         int((1LL * f[1] * Pb2[o[2]] + o[1]) % mod)
+                    , f[2] + o[2]};
         }
-
-        bool operator<(const Hash &o) const {
-            if (size == o.size) return code < o.code;
-            return size < o.size;
-        }
-        bool operator==(const Hash &o) const {
-            return size == o.size && code == o.code;
-        }
-        bool operator!=(const Hash &o) const {
-            return size != o.size || code != o.code;
-        }
+#undef me
     };
 
     struct HashRange {
@@ -499,18 +488,18 @@ namespace RollingHash {
             }
         }
         Hash get(int l, int r) {
-            if(l > r) return Hash();
+            if(l > r) return {};
             if(!l) return p[r];
-            return Hash({(p[r].code.first - p[l - 1].code.first * 1LL * Pb1[r - l + 1] % mod + mod) % mod,
-                         (p[r].code.second - p[l - 1].code.second * 1LL * Pb2[r - l + 1] % mod + mod) % mod}
-                    , r - l + 1);
+            return array{int((p[r][0] - p[l - 1][0] * 1LL * Pb1[r - l + 1] % mod + mod) % mod),
+                         int((p[r][1] - p[l - 1][1] * 1LL * Pb2[r - l + 1] % mod + mod) % mod)
+                    , r - l + 1};
         }
         Hash inv(int l, int r) {
-            if(l > r) return Hash();
+            if(l > r) return {};
             if(r + 1 == s.size()) return s[l];
-            return Hash({(s[l].code.first - s[r + 1].code.first * 1LL * Pb1[r - l + 1] % mod + mod) % mod,
-                         (s[l].code.second - s[r + 1].code.second * 1LL * Pb2[r - l + 1] % mod + mod) % mod}
-                    , r - l + 1);
+            return array{int((s[l][0] - s[r + 1][0] * 1LL * Pb1[r - l + 1] % mod + mod) % mod),
+                         int((s[l][1] - s[r + 1][1] * 1LL * Pb2[r - l + 1] % mod + mod) % mod)
+                    , r - l + 1};
         }
     };
 }
